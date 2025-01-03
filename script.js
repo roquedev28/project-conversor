0d19f2e453b10b0945ae9b89ce3dd6bfaa449277
RoqueDev28 <roquedev23@gamil.com>
2025-01-03 00:22:09 -0300

    versão final

--- script.js
+++ script.js
@@ -47,16 +47,28 @@ function convertValues(){
 }
 function changeCurrency(){
     const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
 
 if(currencySelect.value == "dolar"){
     currencyName.innerHTML = "Dólar americano"
+    currencyImage.src = "./assets/estados-unidos.png"
 }
 
 if(currencySelect.value == "euro"){
     currencyName.innerHTML = "Euro"
+    currencyImage.src = "./assets/euro.png"
+}
+if(currencySelect.value == "libra"){
+    currencyName.innerHTML = "Libra"
+    currencyImage.src = "./assets/libra.png"
 }
 
+if(currencySelect.value == "bitcons"){
+    currencyName.innerHTML = "Bitcons"
+    currencyImage.src = "./assets/bitcoin.png"
+}
 
+convertValues()
 }
 
 currencySelect.addEventListener("change", changeCurrency)
